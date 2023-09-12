b8fa6a13fdbfc7a345f19f7de5fd0791866ed545
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-09-12 14:10:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,73 @@
-console.dir(document);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.all);
-console.log(document.doctype);
-console.log(document.links);
-console.log(document.images);
+var form = document.getElementById("addForm");
 
-const title = document.querySelector('#main-heading');
+var itemList = document.getElementById('items');
 
-title.style.color='green';
-title.style.fontWeight ='bold';
-title.style.border = "thick solid black";
+var item = document.getElementById('item');
+var item1 = document.getElementById('item1');
 
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-/*headerTitle.textContent="HII";
-headerTitle.innerText="Hello";
-*/
-console.log(headerTitle.textContent)
+form.addEventListener('submit', addItem);
 
-var header = document.getElementById('main-header');
-header.style.borderBottom='solid 3px black';
+function addItem(e){
+	e.preventDefault();
+	console.log(1);
+	var item = document.getElementById('item');
+	var item1 = document.getElementById('item1');
+	
+	var li = document.createElement('li');
 
-var items = document.getElementsByClassName('list-group');
-console.log(items);
+	li.className='list-group-item';
+	li.appendChild(document.createTextNode(item.value+" "+item1.value));
+	
+	
+	//delete button 
+	var delbutton  = document.createElement('button');
+	delbutton.className= 'btn btn-danger btn-sm float-right delete';
+	//now add x for the delete button
+	delbutton.appendChild(document.createTextNode('x'));
+	li.appendChild(delbutton);
+
+	var editButton = document.createElement('button');
+	editButton.className='btn  btn-sm float-right edit mr-3';
+	editButton.appendChild(document.createTextNode('EDIT'));
+	li.appendChild(editButton);
+
+	itemList.appendChild(li);
+}
+
+itemList.addEventListener('click',removeItem);
+
+function removeItem(e){
+	e.preventDefault();
+
+	if(e.target.classList.contains('delete')){
+		var li = e.target.parentElement;
+		itemList.removeChild(li);
+	}
+}
+
+
+//filter
+
+var filter = document.getElementById('filter');
+
+filter.addEventListener('keyup',filterItems);
+
+function filterItems(e){
+	e.preventDefault();
+
+	var text = e.target.value.toLowerCase();
+	console.log(text);
+
+	var items = itemList.getElementsByTagName('li');
+
+	Array.from(items).forEach(function(item){
+		var itemName = item.firstChild.textContent;
+		
+		if(itemName.toLowerCase().indexOf(text)!=-1 ){
+			item.style.display='block;'
+		}
+		else{
+			item.style.display='none';
+		}
+	})
+}
